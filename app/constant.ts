02676e5243c476aa862826f52918a0d5f573a2b7
paijishult <118252866+paijishult@users.noreply.github.com>
2024-09-25 18:05:07 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -502,5 +502,5 @@ export const PLUGINS = [
   { name: "Search Chat", path: Path.SearchChat },
 ];
 
-export const SAAS_CHAT_URL = "https://nextchat.dev/chat";
-export const SAAS_CHAT_UTM_URL = "https://nextchat.dev/chat?utm=github";
+export const SAAS_CHAT_URL = "https://chat.pai.al";
+export const SAAS_CHAT_UTM_URL = "https://chat.pai.al";
