a540b80efba50ceb6bb295fa9041d3c918fb7543
paijishult <118252866+paijishult@users.noreply.github.com>
2024-09-25 17:48:29 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "偏爱AI助手",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "偏爱AI助手",
     statusBarStyle: "default",
   },
 };
